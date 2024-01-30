2a9d7a46702d81a4f2793067e5ca6bcab5fc95c0
devops-prince <dev.prince116@gmailcom>
2024-01-30 18:04:52 +0530

    some line of codes are added

---
+++ modal/script.js
@@ -0,0 +1,27 @@
+let shareProfile = document.getElementById("share-button");
+let overlay = document.getElementById("overlay");
+let modal = document.getElementById("modal");
+let remove= 
+
+
+// let openModal = function (){
+//     modal.classList.add("active");
+// };
+
+shareProfile.addEventListener("click",function(){
+    let modal = document.getElementById("modal");
+    modal.style.cssText= "scale: 1;"
+
+});
+
+
+
+// shareProfile.addEventListener("click",function(){
+//     let modal = document.getElementById("modal");
+//     modal.style.cssText= "scale: 0;"
+
+// });
+
+
+
+
